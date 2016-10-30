c2e06ab9db2cb53e87f855f6ee22cf8db4ac18ca
Samvid95 <samvid95@gmail.com>
2016-10-30 20:56:37 +0530

    Now 16 part is defined!!

--- PointDistance/PointDistance/PointDistance.cpp
+++ PointDistance/PointDistance/PointDistance.cpp
@@ -10,6 +10,8 @@
 using namespace std;
 
 class Point {
+private:
+	unsigned short int part16;
 public:
 	double xCoord, yCoord;
 	Point() {
@@ -29,14 +31,65 @@ public:
 			angle = angle + 360;
 		}
 		cout << "The angle after compensating everything is(final): " << angle << endl;
+		
+		if (angle < 0 && angle <= 22.5) {
+			part16 = 1;
+		}
+		else if (angle < 22.5 && angle <= 45) {
+			part16 = 2;
+		}
+		else if (angle < 45 && angle <= 67.5) {
+			part16 = 3;
+		}
+		else if (angle < 67.5 && angle <= 90) {
+			part16 = 4;
+		}
+		else if (angle < 90 && angle <= 112.5) {
+			part16 = 5;
+		}
+		else if (angle < 112.5 && angle <= 135) {
+			part16 = 6;
+		}
+		else if (angle < 135 && angle <= 157.5) {
+			part16 = 7;
+		}
+		else if (angle < 157.5 && angle <= 180) {
+			part16 = 8;
+		}
+		else if (angle < 180 && angle <= 202.5) {
+			part16 = 9;
+		}
+		else if (angle < 202.5 && angle <= 225) {
+			part16 = 10;
+		}
+		else if (angle < 225 && angle <= 247.5) {
+			part16 = 11;
+		}
+		else if (angle < 247.5 && angle <= 270) {
+			part16 = 12;
+		}
+		else if (angle < 270 && angle <= 292.5) {
+			part16 = 13;
+		}
+		else if (angle < 292.5 && angle <= 315) {
+			part16 = 14;
+		}
+		else if (angle < 315 && angle <= 337.5) {
+			part16 = 15;
+		}
+		else if (angle < 337.5 && angle <= 360) {
+			part16 = 16;
+		}
+		cout << part16 << endl;
+
 		return angle;
 	}
 };
 
 int main()
 {
-	Point p1(4, 5);
-	Point p2(7, 5);
+	Point p1(0,0);
+	Point p2(-2,-9);
 
 	float answer = p1.findAngle(p2);
 	cout << "2nd time:" << endl;;
