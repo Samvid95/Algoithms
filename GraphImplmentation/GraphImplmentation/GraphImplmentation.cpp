1ca92c3c80d6b21768426a3c3a79b9cdf569e9fa
Samvid95 <samvid95@gmail.com>
2016-12-01 21:07:28 +0530

    BFS implementation done!

--- GraphImplmentation/GraphImplmentation/GraphImplmentation.cpp
+++ GraphImplmentation/GraphImplmentation/GraphImplmentation.cpp
@@ -1,14 +1,21 @@
 /*
 // GraphImplmentation.cpp : Defines the entry point for the console application.
 // Made By: Samvid Jhaveri
+//			Monil Shah
+//			Vatsal Gopani
 // This is the simple Graph Implementation; I think so many points are left to implemented!!
 */
 #include "stdafx.h"
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <iterator>
 
+#define ROWS 20
+#define COLS 20
+
 using namespace std;
+
 /*
 Structure Name: Vertex
 Params: Parent, Child, ParentList and ChildList; Value, Color, Weight
@@ -24,6 +31,8 @@ struct Vertex {
 	int value;
 	int color;
 	int weight;
+	int dist;
+	Vertex *top;
 	/*
 	Function name: Default constructor
 	*/
@@ -35,7 +44,6 @@ struct Vertex {
 		weight = NULL;
 		parentList.clear();
 		childList.clear();
-
 	}
 	/*
 	Function name: Constructor which initialize the value as defined in the parameter.
@@ -68,7 +76,7 @@ class Graph {
 private:
 	vector<Vertex *> vertexList;
 	
-	int matrix[20][20];
+	int matrix[ROWS][COLS];
 	/*
 	Name: FindNodeInternal
 	Parameters: int value 
@@ -92,6 +100,7 @@ private:
 		}
 		return 0;
 	}
+
 	void AllChilds() {
 		for (int i = 0; i < vertexList.size(); i++) {
 			cout << "The vertex is: " << vertexList[i]->value << endl;
@@ -152,11 +161,21 @@ public:
 	This function gives the output value of the whole graph vertices' values
 	*/
 	void PrintAll() {
+		cout << "\n\n";
 		for (int i = 0; i < vertexList.size(); i++) {
-			cout << "The value of the vertex is: " << vertexList[i]->value << endl;
+			cout << "The value of the vertex is: " << vertexList[i]->value << " and color is: " << vertexList[i]->color << " and dist: " << vertexList[i]->dist<< endl;
 		}
 	}
 
+	void InitilizeBFS(int value) {
+		for (int i = 0; i < vertexList.size(); i++) {
+			if (vertexList[i]->value != value) {
+				vertexList[i]->color = 0;
+				vertexList[i]->dist = -10;
+				vertexList[i]->top = NULL;
+			}
+		}
+	}
 	/*
 	Name: FindNode
 	Parameters: int value
@@ -167,6 +186,15 @@ public:
 		for (int i = 0; i < vertexList.size(); i++) {
 			if (vertexList[i]->value == value) {
 				return true;
+			}
+		}
+		return false;
+	}
+
+	Vertex* FindNodeBFS(int value) {
+		for (int i = 0; i < vertexList.size(); i++) {
+			if (vertexList[i]->value == value) {
+				return vertexList[i];
 			}
 		}
 		return false;
@@ -195,8 +223,15 @@ public:
 		Vertex *parentNode = FindNodeInternal(parent);
 		node->parentList.push_back(parentNode);
 		parentNode->childList.push_back(node);
+		
 	}
 	
+	/*
+	Name: CreateAdjecency Matrix
+	Parameters: NOne
+	Return Type: None
+	This program will scavange every node of the graph and according to it will create a adjecency matrix.
+	*/
 	void CreateAdjencyMatrix() {
 		for (int i = 0; i < vertexList.size(); i++) {
 			for (int j = 0; j < vertexList.size(); j++) {
@@ -214,15 +249,41 @@ public:
 		}
 		
 	}
-
+	/*
+	Name: CreateAdjencyMatrix
+	Parameter: int arr[][] (2-D Array), int size
+	Return Type: None
+	This is an overloaded method which create the adjency matrix of this graph. 
+	*/
+	void CreateAdjencyMatrix(int arr[ROWS][COLS], int size) {
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				matrix[i][j] = arr[i][j];
+			}
+		}
+	}
+	/*
+	Name: SearchEdge
+	Parameters: int value and int parent
+	Return Type: int value
+	This program will return an edge's weight if it exists.
+	*/
 	int SearchEdge(int value, int parent) {
 		int m = FindNodeIndexInternal(value);
-		cout << "The value is: " << m << endl;
+		Vertex *mm = FindNodeInternal(value);
+		//cout << "THe size of the child list is: " << mm->childList.size();
+		//cout << "The value is: " << m << endl;
 		int n = FindNodeIndexInternal(parent);
-		cout << "The parent's value is: " << n << endl;
+		//cout << "The parent's value is: " << n << endl;
 		return matrix[m][n];
 	}
 
+	/*
+	Name: PrintAdjecencyMatrix
+	Parameters: None
+	Return Type: None
+	This functin will print the adjecency matrix of the graph.
+	*/
 	void PrintAdjencyMatrix() {
 		cout << "\n\n";
 		for (int i = 0; i < vertexList.size(); i++) {
@@ -237,10 +298,81 @@ public:
 		}
 	}
 };
+/*
+Name: CreateGraphbyMatrix
+Parameters: None
+ReturnType: Graph*
+This is an alternative and faster method to create graph by supplying the adjecency matrix values.
+*/
+Graph* CreateGrpahbyMatrix() {
+	vector<int> values;
+	int n;
+	cout << "Enter the number of vertices:";
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int temp;
+		cout << "Enter the value of Vertex " << i << " : ";
+		cin >> temp;
+		values.push_back(temp);
+	}
+	cout << "\n\n";
+	int tempMatrix[ROWS][COLS];
+	cout << "Now enter the matrix value row wise: " << endl;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << "Enter value for element[" << i << "][" << j <<"]: ";
+			cin >> tempMatrix[i][j];
+		}
+		cout << "\n";
+	}
+
+	Graph *gAd = new Graph(values[0]);
+	for (int i = 1; i < n; i++) {
+		gAd->CreateNode(values[i]);
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (tempMatrix[i][j] != 0) {
+				gAd->CreateEdge(values[j], values[i]);
+			}
+		}
+	}
+
+	gAd->CreateAdjencyMatrix(tempMatrix, n);
+
+	return gAd;
+}
+
+void BFS(Graph *gh, int value) {
+	gh->InitilizeBFS(value);
+	Vertex *s = gh->FindNodeBFS(value);
+	s->color = 1;
+	s->dist = 0;
+	s->top = NULL;
+	queue<Vertex *> Queue;
+	Queue.push(s);
+	while (!Queue.empty())
+	{
+		Vertex *temp = Queue.front();
+		for (int i = 0; i < temp->childList.size(); i++) {
+			Vertex *v = temp->childList[i];
+			if (v->color == 0) {
+				v->color = 1;
+				v->dist = temp->dist + 1;
+				v->top = temp;
+				Queue.push(v);
+			}
+		}
+		temp->color = 2;
+		Queue.pop();
+	}
+}
 
 
 int main()
 {
+	/*
 	Graph *gh = new Graph(13); //Generate a graph
 
 	gh->CreateNode(8);
@@ -259,8 +391,17 @@ int main()
 
 	gh->CreateAdjencyMatrix();
 	gh->PrintAdjencyMatrix();
+	*/
 	
+	Graph *gh;
+	gh = CreateGrpahbyMatrix();
+	BFS(gh, 3);
+	gh->PrintAll();
 
+	/*
+	BFS(gh, 3);
+	gh->PrintAll();
+	*/
 	return 0;
 }
 
